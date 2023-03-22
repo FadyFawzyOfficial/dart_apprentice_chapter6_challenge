8bd66f1d1c0f6a98485da77952a8f5fe3184529e
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-03-22 12:53:30 +0200

    6.2. Challenge 2: Spheres

--- bin/challenge.dart
+++ bin/challenge.dart
@@ -3,6 +3,10 @@ void main() {
   print(bert);
   final ernie = Student(firstName: 'Ernie', lastName: 'Fady', grade: 85);
   print(ernie);
+
+  final sphere = Sphere(radius: 12);
+  print('Volume: ${sphere.volume}');
+  print('Area: ${sphere.area}');
 }
 
 /// Challenge 1: Bert and Ernie
@@ -27,4 +31,25 @@ class Student {
   @override
   String toString() =>
       'Student(firstName: $firstName, lastName: $lastName, grade: $grade)';
+}
+
+/// Challenge 2: Spheres
+///
+/// Create a `Sphere` class with a `const` constructor that takes a positive
+/// length `radius` as a named parameter. Add getters for the the volume and
+/// surface area but none for the radius. Don't use the `dart:math` package
+/// but store your own version of `pi` as a `static` constant. Use your class
+/// to find the volume and surface area of a sphere with a radius of 12.
+class Sphere {
+  static const pi = 22 / 7;
+
+  final double _radius;
+
+  const Sphere({required double radius})
+      : assert(radius > 0),
+        _radius = radius;
+
+  double get volume => 4 / 3 * pi * _radius * _radius * _radius;
+
+  double get area => 4 * pi * _radius * _radius;
 }
